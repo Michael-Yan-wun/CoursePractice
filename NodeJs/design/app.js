167264d0d1d6e128bfdd83e1fe00d9cac3f313eb
Lin Michael <yanwun1214@gmail.com>
2021-03-09 09:29:08 +0800

    Summary
    #comment 修改會員登入與登入邏輯

--- NodeJs/design/app.js
+++ NodeJs/design/app.js
@@ -23,7 +23,7 @@ app.listen(3000);
 //       接著新增views資料夾接著將index.html搬入views，最後把副檔名改成.ejs。
 //       index.ejs將檔案內的路徑修改
 
-// localhost:3000/home 首頁
+//首頁
 app.get("/home", function (req, res) {
     let who = "Guest";
     if (req.session.userName) {
@@ -31,26 +31,43 @@ app.get("/home", function (req, res) {
     }
     res.render("index.ejs", { userName: who });
 });
-// localhost:3000/member/login 登入頁面
+
+//登入頁面
 app.get("/member/login", function (req, res) {
     res.render("login.ejs", {});
 });
 
 // 將使用者送來的資料送出
 app.post("/member/login", function (req, res) {
-    let data = req.body.txtID;      // 取得使用者輸入
-    req.session.userName = data;    // 將資料塞進session
-    // res.send("i got form data  " + data);
-    res.redirect("/home");         // 重新導向回首頁
+    let data = req.body.txtID; // 取得使用者輸入
+    if (data) {
+        req.session.userName = data; // 將資料塞進session
+        res.redirect("/home"); // 重新導向回首頁
+    } else {
+        res.render("index.ejs", {}); // 若沒輸入則返回index頁面
+    }
 });
 // 清空session
 app.get("/member/signout", function (req, res) {
-    delete req.session.userName;
-    // req.session.destroy;
-    res.redirect("/home");         // 重新導向回首頁
+    delete req.session.userName; //刪除session的屬性
+    res.redirect("/home"); // 重新導向回首頁
 });
 
-// localhost:3000/member/secret 會員專屬頁面
+//會員專屬頁面
 app.get("/member/secret", function (req, res) {
-    res.render("member.ejs", {});
+    // 先檢查session內是否有userName
+    if (!req.session.userName) {
+        res.redirect("/home");
+        return;
+    }
+    // 若session中有username則回傳who變數
+    let who = "Guest";
+    if (req.session.userName) {
+        who = req.session.userName;
+    }
+    if (who == "Guest") {
+        res.redirect("/home");
+        return;
+    }
+    res.render("secret.ejs", { userName: who });
 });
